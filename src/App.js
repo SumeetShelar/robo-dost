d688a9581137b43f4765dfbeb004e9770f306de0
SumeetShelar <56186357+SumeetShelar@users.noreply.github.com>
2021-02-07 19:18:31 +0530

    making it more realistic

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import './App.css'
-import { robots } from './robots';
 
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ""
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
@@ -21,13 +26,17 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robo => {
             return robo.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-        return(
-            <div className="tc">
-                <h1 className="f1">RoboDost</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={ filteredRobots } />
-            </div>
-        )
+        if (this.state.robots.length === 0){
+            return <h1>LOADING....</h1>
+        }else {
+            return(
+                <div className="tc">
+                    <h1 className="f1">RoboDost</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={ filteredRobots } />
+                </div>
+            )
+        }
     }
 }
 
